e62a34aaa65c33a35a8304f1846452b895b623ae
shehab-fekry <shehabeddin99@gmail.com>
2025-12-01 15:05:33 +0200

    add express middleware for CORS, cookie parsing, logging, and rate limiting; update routes and ports for auth and api-gateway services

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,21 +1,47 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
-import * as path from 'path';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import proxy from 'express-http-proxy';
 
 const app = express();
+// allow localhost:3000 to access resources from this server
+app.use(cors({
+origin: 'http://localhost:3000',
+allowedHeaders: ['Content-Type', 'Authorization'],
+credentials: true,
+}))
+// log request info (logger)
+app.use(morgan('dev'));
+// control request body size
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb', extended: true}));
+// parse cookies
+app.use(cookieParser());
+// ...
+app.set('trust proxy', 1);
+// set limit on requests from same IP
+app.use(rateLimit({
+windowMs: 15 * 60 * 1000, // remember request for 15 minutes
+max: (req: any) => req.user ? 1000 : 100, // if the user is (logged in) limit his IP to 100 requests, else limit each IP to 1000 requests per windowMs
+standardHeaders: true, // return rate limit info in the `RateLimit-*` headers
+legacyHeaders: true, // enable the `X-RateLimit-*` headers
+keyGenerator: (req: any) => req.ip, // limit based on IP address
+}));
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api-gateway!' });
+// proxy requests to [...]-service
+app.use('/auth', proxy('http://localhost:6001'));
+// routes
+app.get('/gateway-health', (req, res) => {
+res.send({ message: 'Welcome to api-gateway!' });
 });
 
-const port = process.env.PORT || 3333;
+// server's port
+const port = process.env.PORT || 8080;
+// start the server
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -1,21 +1,15 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
-import * as path from 'path';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to auth-service!' });
+app.get('/signin', (req, res) => {
+res.send({ message: 'Welcome, Signin to auth-service!' });
 });
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 6001;
+
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
